3c6a79746d8178a55e4230bf57e5a96833e590a5
rumazius <rumazius@mail.ru>
2021-09-10 07:58:46 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+std::vector<int> kmp(const std::string &s) {
+    size_t n = s.size();
+    std::vector<int> p(n);
+
+    for (size_t i = 1; i < n; i++) {
+        size_t j = p[i - 1];
+        while (j > 0 && s[i] != s[j]) {
+            j = p[j - 1];
+        }
+        if (s[i] == s[j]) {
+            j++;
+        }
+        p[i] = j;
+    }
+    return p;
+}
+
+int main() {
+
+    std::string s;
+    std::cin >> s;
+
+    std::vector<int> p = kmp(s);
+
+    for (int i : p) {
+        std::cout << i << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
